7fe716982d08a4b5430765689933ac87d58c3696
defnngj <defnngj@gmail.com>
2022-11-07 00:13:43 +0800

    version: 3.0

---
+++ assets/page_object.html.882c2107.js
@@ -0,0 +1,1 @@
+const e={key:"v-299549e4",path:"/app-testing/page_object.html",title:"Page Object\u8BBE\u8BA1\u6A21\u5F0F",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1667322855e3,contributors:[{name:"defnngj",email:"defnngj@gmail.com",commits:2}]},filePathRelative:"app-testing/page_object.md"};export{e as data};
